1093d5494f84f248ab28164e8d86b39d2b692bd8
Robert <robertniyompuhwe@gmail.com>
2026-03-11 12:09:24 +0200

    some css was added

--- src/components/like.tsx
+++ src/components/like.tsx
@@ -1,8 +1,53 @@
+import { Play, MoreHorizontal } from "lucide-react"
 
 function LikedMost(){
     return(
         <div className="liked-container">
-
+            <h3>you might like</h3>
+       <div className="liked-container-item">
+        <div className="dots">
+        <MoreHorizontal/>
+        </div>
+        <h4>fantasy</h4>
+        <div className="liked-description">
+        <h6>The flash</h6>
+        <p>Barry align use his super speed to change the pastsdfhsjhfposjfsdjfoisdfopisdnpsdajfosdfasdjfpodshjfoisdo</p>
+        </div>
+        <Play size={40}  className="play" fill="black"/>
+       </div>
+       <div className="liked-container-item">
+        <div className="dots">
+        <MoreHorizontal/>
+        </div>
+         <h4>fantasy</h4>
+        <div className="liked-description">
+        <h6>The flash</h6>
+        <p>Barry align use his super speed to change the pastsdfhsjhfposjfsdjfoisdfopisdnpsdajfosdfasdjfpodshjfoisdo</p>
+        </div>
+        <Play size={40} className="play" fill="black"/>
+       </div>
+       <div className="liked-container-item">
+        <div className="dots">
+        <MoreHorizontal/>
+        </div>
+         <h4>fantasy</h4>
+        <div className="liked-description">
+        <h6>The flash</h6>
+        <p>Barry align use his super speed to change the pastsdfhsjhfposjfsdjfoisdfopisdnpsdajfosdfasdjfpodshjfoisdo</p>
+        </div>
+        <Play size={40}  className="play" fill="black"/>
+       </div>
+        <div className="liked-container-item">
+            <div className="dots">
+            <MoreHorizontal/>
+            </div>
+        <h4>fantasy</h4>
+        <div className="liked-description">
+        <h6>The flash</h6>
+        <p>Barry align use his super speed to change the pastsdfhsjhfposjfsdjfoisdfopisdnpsdajfosdfasdjfpodshjfoisdo</p>
+        </div>
+        <Play size={40}  className="play" fill="black"/>
+       </div>
         </div>
     )
 }
--- src/components/sideBar.tsx
+++ src/components/sideBar.tsx
@@ -1,14 +1,85 @@
-
+import { Play}from "lucide-react"
  function Sidebar(){
   return(
     <div className="sidebar">
 
-          <div className="sidebar-trending">
-          <div className="sidebar-trending-item"></div>
-          </div>
-          <div className="sidebar-continue"></div>  
+            <div className="sidebar-trending">
+            <h5>New Trending</h5>
+             <div className="sidebar-trending-item">
 
-          
+        <div className="sidebar-trending-item-description">
+        <p>Barry align use his super speed to change the pastsdfhsjhfposjfsdjfoisdfopisdnpsdajfosdfasdjfpodshjfoisdo</p>
+        </div>
+        <Play size={40}  className="play" fill="black"/>
+       </div>
+       <div className="sidebar-trending-item">
+        
+        <div className="sidebar-trending-item-description">
+        
+        <p>Barry align use his super speed to change the pastsdfhsjhfposjfsdjfoisdfopisdnpsdajfosdfasdjfpodshjfoisdo</p>
+        </div>
+        <Play size={40} className="play" fill="black"/>
+       </div>
+       <div className="sidebar-trending-item">
+        <div className="dots">
+        
+        </div>
+    
+        <div className="sidebar-trending-item-description">
+      
+        <p>Barry align use his super speed to change the pastsdfhsjhfposjfsdjfoisdfopisdnpsdajfosdfasdjfpodshjfoisdo</p>
+        </div>
+        <Play size={40}  className="play" fill="black"/>
+       </div>
+        <div className="sidebar-trending-item">
+            <div className="dots">
+            
+            </div>
+    
+        <div className="sidebar-trending-item-description">
+      
+        <p>Barry align use his super speed to change the pastsdfhsjhfposjfsdjfoisdfopisdnpsdajfosdfasdjfpodshjfoisdo</p>
+        </div>
+        <Play size={40}  className="play" fill="black"/>
+       </div>
+        </div>
+         <div className="sidebar-continue">
+  <h2>Continue Watching</h2>
+
+  <div className="sidebar-continue-item">
+    <img className="sidebar-continue-item-thumbnail" src="dark.jpg" alt="Dark" />
+    <div className="sidebar-continue-item-info">
+      <h4>Dark Season 3</h4>
+      <span>Episode 3</span>
+    </div>
+    <div className="sidebar-continue-item-play">
+      <Play size={18} fill="white" color="white" />
+    </div>
+  </div>
+
+  <div className="sidebar-continue-item">
+    <img className="sidebar-continue-item-thumbnail" src="transformers.jpg" alt="Transformers" />
+    <div className="sidebar-continue-item-info">
+      <h4>Transformers - Da...</h4>
+      <span>32min 12sec</span>
+    </div>
+    <div className="sidebar-continue-item-play">
+      <Play size={18} fill="white" color="white" />
+    </div>
+  </div>
+
+  <div className="sidebar-continue-item">
+    <img className="sidebar-continue-item-thumbnail" src="lupin.jpg" alt="Lupin" />
+    <div className="sidebar-continue-item-info">
+      <h4>Lupin Season 2</h4>
+      <span>Episode 2</span>
+    </div>
+    <div className="sidebar-continue-item-play">
+      <Play size={18} fill="white" color="white" />
+    </div>
+  </div>
+
+</div>
 
     </div>
   )
